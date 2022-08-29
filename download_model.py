6f0d811cc22865ea42b2f2697a15cfe7e7a7d175
Vilhelm Frändberg <vilhelm.frandberg@gmail.com>
2022-08-29 15:02:40 +0200

    Fix indentation

--- download_model.py
+++ download_model.py
@@ -6,10 +6,10 @@ TOKEN = os.environ.get("TOKEN", None)
 fp16 = bool(os.environ.get("FP16", True))
 
 pipe = StableDiffusionPipeline.from_pretrained(
-	"CompVis/stable-diffusion-v1-4",
+    "CompVis/stable-diffusion-v1-4",
     revision="fp16" if fp16 else None,
     torch_dtype=torch.float16 if fp16 else None,
-	use_auth_token=TOKEN,
+    use_auth_token=TOKEN,
 )
 
 pipe.save_pretrained('./models/stable-diffusion-v1-4')
